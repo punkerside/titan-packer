74f00f46399a27cf8ae55f92c72d16b59b4e9362
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2023-02-19 18:55:42 -0500

    cambiando cmd

--- init.sh
+++ init.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # init ansible
-rm -rf ansible/roles/common/
-ansible-galaxy install punkerside.ansible_ubuntu_common -p common/
-mv common/punkerside.ansible_ubuntu_common/ ansible/roles/common/
-rm -rf common/
+rm -rf ansible/roles/common/ && rm -rf .ansible/roles/punkerside.ansible_ubuntu_common/
+ansible-galaxy install punkerside.ansible_ubuntu_common --force
+mv .ansible/roles/punkerside.ansible_ubuntu_common/ ansible/roles/common/
 
-# init packer
-packer init packer/config.pkr.hcl
+# # init packer
+# packer init packer/config.pkr.hcl
 
-packer build \
-  -var 'project='${PROJECT}'' \
-  -var 'env='${ENV}'' \
-  -var 'service='${SERVICE}'' \
-  -var 'subnet_id='${SUBNET_ID}'' \
-packer/config.pkr.hcl
+# packer build \
+#   -var 'project='${PROJECT}'' \
+#   -var 'env='${ENV}'' \
+#   -var 'service='${SERVICE}'' \
+#   -var 'subnet_id='${SUBNET_ID}'' \
+# packer/config.pkr.hcl
