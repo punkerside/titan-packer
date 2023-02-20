ba5e6e9cb7cdeb49337f30019214607ff464716c
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2023-02-19 21:35:55 -0500

    agregando init

--- init.sh
+++ init.sh
@@ -5,12 +5,12 @@ rm -rf ansible/roles/common/ && rm -rf .ansible/roles/punkerside.ansible_ubuntu_
 ansible-galaxy install punkerside.ansible_ubuntu_common --force
 mv .ansible/roles/punkerside.ansible_ubuntu_common/ ansible/roles/common/
 
-# # init packer
-# packer init packer/config.pkr.hcl
+# init packer
+packer init packer/config.pkr.hcl
 
-# packer build \
-#   -var 'project='${PROJECT}'' \
-#   -var 'env='${ENV}'' \
-#   -var 'service='${SERVICE}'' \
-#   -var 'subnet_id='${SUBNET_ID}'' \
-# packer/config.pkr.hcl
+packer build \
+  -var 'project='${PROJECT}'' \
+  -var 'env='${ENV}'' \
+  -var 'service='${SERVICE}'' \
+  -var 'subnet_id='${SUBNET_ID}'' \
+packer/config.pkr.hcl
